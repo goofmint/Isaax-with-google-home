5e4453cb8454bd5b9f9662c2a31f6b5380ee51ac
Atsushi <atsushi@moongift.jp>
2018-06-25 13:54:26 +0900

    Update

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var language = 'ja'; // if not set 'us' language will be used
 
 googlehome.ip('192.168.0.21', language);
 
-const message = process.env.MESSAGE || 'こんにちは';
+let old_message = '';
 
-googlehome.notify(message, function(res) {
-  console.log(res);
-});
+setInterval(() => {
+  const message = process.env.MESSAGE || 'こんにちは';
+  if (message !== old_message) {
+    googlehome.notify(message, function(res) {
+      console.log(res);
+    });
+    old_message = message;
+  }
+}, 5000)
